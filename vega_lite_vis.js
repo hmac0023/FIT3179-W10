aecb2aa834c80e15595a6c5cf2f7c12807fff8c7
hmac0023 <hmac0023@student.monash.edu>
2024-09-30 21:43:06 +1000

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,6 @@
+var vg_1 = "FIT3179-W9HW.vg.json"; 
+
+vegaEmbed("#choropleth_map", vg_1).then(function(result) {
+     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+     
+}).catch(console.error);
